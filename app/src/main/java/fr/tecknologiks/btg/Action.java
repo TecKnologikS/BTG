253844ed69a69825547562057dc62cc4f11c20a7
Robin <robinpauquet@gmail.com>
2017-02-27 14:23:35 +0100

    Initial commit

---
+++ app/src/main/java/fr/tecknologiks/btg/Action.java
@@ -0,0 +1,11 @@
+package fr.tecknologiks.btg;
+
+/**
+ * Created by robin on 2/27/2017.
+ */
+
+public class Action {
+    public static final int LOAD_URL =  1;
+    public static final int PILLAGE =   2;
+    public static final int EVASION =   3;
+}
---
+++ app/src/main/java/fr/tecknologiks/btg/Commande.java
@@ -0,0 +1,28 @@
+package fr.tecknologiks.btg;
+
+/**
+ * Created by robin on 2/27/2017.
+ */
+
+public class Commande {
+
+    private static int action = 0;
+    private static String info_comp = "";
+    private static boolean onAttack = false;
+    private static int minute = 0;
+    private static int village = 0; //0 --> Tous
+
+}
+
+/***
+
+
+ Pillage liste 1
+ info comp : ID liste pillage
+ onattack false
+ action : 2
+ minute : 10
+ village : id village
+
+
+ ***/
